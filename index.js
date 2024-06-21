85803b64b4a8d50e76f0dce17ce2d2bab5634031
achyutthapa7 <achyut777thapa@gmail.com>
2024-06-21 14:05:16 +0545

    commiting changes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 
 //middlewares
 
-app.use(cors({ credentials: true }));
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
