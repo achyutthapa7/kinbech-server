8e67c3953025dfbd5dfbd7d4add12616e2fef5c8
achyutthapa7 <achyut777thapa@gmail.com>
2024-06-23 12:50:59 +0545

    v

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ app.use(
     allowedHeaders: "Content-Type,Authorization",
   })
 );
+app.options("*", cors());
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
