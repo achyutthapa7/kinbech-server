4d99ae8c232cc7ad1466a38e91ae3a911d586c4f
Achyut <achyut777thapa@gmail.com>
2024-08-04 19:38:51 +0545

    bug fixes

--- index.mjs
+++ index.mjs
@@ -27,7 +27,7 @@ app.use(cookieParser());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use("/", router);
-app.listen(port, () => {
+app.listen(port, (req,res) => {
   console.log(
     `Server is live at: ${req.protocol}://${req.get("host")}:${req.get("PORT")}`
   );
