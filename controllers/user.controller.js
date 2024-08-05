f90a85e7678a35a964872d17f9356d43dcc8b6b5
Achyut <achyut777thapa@gmail.com>
2024-08-05 14:49:39 +0545

    cookies

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -149,6 +149,9 @@ export async function login(req, res) {
       res.cookie("token", token, {
         maxAge: 3 * 24 * 60 * 60 * 1000,
         httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "None",
+        path: "/",
       });
       await userModel.findOneAndUpdate(
         {
